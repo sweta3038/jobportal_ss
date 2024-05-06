aea11cb19ca08bfcc3750448846ea059c307698e
sweta <swetasinha599@gmail.com>
2024-05-05 23:57:38 -0700

    Ready to deploy

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app=express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+var cors=require('cors');
+const errorHandler = require("./middleware/error");
+const authRoutes =require('./routes/authRoutes');
+const userRoutes =require('./routes/userRoutes');
+const jobTypeRoutes =require('./routes/jobsTypeRoutes');
+const jobRoutes = require("./routes/jobsRoutes");
+
+//database connection
+mongoose.connect(process.env.DATABASE , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log("DB connected"))
+   .catch((err) => console.log(err));
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/api',authRoutes);
+app.use('/api',userRoutes);
+app.use('/api',jobTypeRoutes);
+app.use('/api',jobRoutes);
+//error handler
+app.use(errorHandler);
+
+//routes
+// app.get('/',(req,res)=>{
+//         res.send("Hello from node js");
+//})
+
+//port
+const port = process.env.PORT || 8000
+
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}`);
+});
